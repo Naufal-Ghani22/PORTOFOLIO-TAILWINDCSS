be5c77635807c202ea8aef933ac51b9358bd4afd
Naufal Ghani <24082010162@student.upnjatim.ac.id>
2025-03-04 16:15:13 +0700

    navbar clear

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center:true,
+      padding:'16px',
+    },
+
+    extend: {
+      colors:{
+        primary:'#3D8D7A',
+        second:'#27445D',
+        third:'#00DF9A',
+        text:'#64748b',
+      },
+
+      screens:{
+        '2xl': '1720px',
+      }
+    },
+
+
+  },
+  plugins: [],
+}
+
